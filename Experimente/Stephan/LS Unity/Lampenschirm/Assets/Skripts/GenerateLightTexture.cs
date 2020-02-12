2295ee1622e0cf2db81187f94d531e86e4dbfd76
soapsurfer <sebastian.vogt@soapsurfer.de>
2020-02-12 12:13:26 +0100

    Fix generated texture

--- Experimente/Stephan/LS Unity/Lampenschirm/Assets/Skripts/GenerateLightTexture.cs
+++ Experimente/Stephan/LS Unity/Lampenschirm/Assets/Skripts/GenerateLightTexture.cs
@@ -8,14 +8,12 @@ public class GenerateLightTexture : MonoBehaviour
     {
         Texture2D texture = new Texture2D(2200, 1200, TextureFormat.RGBA32,  false,  false); 
         
-            
-            
             for (int y = 0; y < texture.height; y++)
             {
                 for (int x = 0; x < texture.width/2; x++) // texture1.width
                 {
-                    float transparency = calcTransparency(x, radiusCylinder, distBeamer);
-                    Color color_transparent = new Color(0f, 0f, 0f, transparency);
+                    float transparency = calcTransparency((float)x, radiusCylinder, distBeamer);
+                    Color color_transparent = new Color(0f, 0f, 0f, checkThreshold(transparency));
                     
 
                     texture.SetPixel(x, y, color_transparent);
@@ -23,27 +21,28 @@ public class GenerateLightTexture : MonoBehaviour
                     
                 }
             }
+            
             texture.Apply();
             return texture;
     }
 
     static float calcTheta(float radius, float distance, float angleAlpha)
     {
-      float alpha = angleAlpha * Mathf.PI / 180;
+      float alpha = angleAlpha * Mathf.PI / 180f;
       float x = Mathf.Sin(alpha) * radius;
       float p = Mathf.Cos(alpha) * radius;
       float phi = Mathf.Tan(x/(distance + radius - p));
-      alpha = alpha * 180 / Mathf.PI;
-      phi = phi * 180 / Mathf.PI;
-      float beta = 180 - alpha - phi;
-      float theta = 180 - beta;
+      alpha = alpha * 180f / Mathf.PI;
+      phi = phi * 180f / Mathf.PI;
+      float beta = 180f - alpha - phi;
+      float theta = 180f - beta;
       return theta;
     }
 
     static float pixelToAngle(float x)
     {
-      int max_angle = 120;
-      int max_pixel = 2200;
+      float max_angle = 120f;
+      float max_pixel = 2200f;
 
       x = Mathf.Abs(x);
       float ratio = max_angle/max_pixel;
@@ -54,8 +53,16 @@ public class GenerateLightTexture : MonoBehaviour
     {
       float x_angle = pixelToAngle(x);
       float theta = calcTheta(radiusCylinder, distBeamer, x_angle);
-      float bright_perc = Mathf.Cos(theta*Mathf.PI/180);
+      float bright_perc = Mathf.Cos(theta*Mathf.PI/180f);
       float transparency = (1-bright_perc);
       return transparency;
+    }
+
+    static float checkThreshold(float t)
+    {
+      if(t > 0.4f)
+        return 0.4f;
+      else
+        return t;
     }
 }
